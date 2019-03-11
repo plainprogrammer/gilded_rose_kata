44cd7da0979203fee10f5a4ec45c3d4dba1d5f88
Kyle Lemon <james.kyle.lemon@gmail.com>
2019-03-11 14:58:05 -0400

    split unwieldy #update_quality into four cleaner methods

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,53 +1,50 @@
 def update_quality(items)
   items.each do |item|
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
-      if item.quality > 0
-        if item.name != 'Sulfuras, Hand of Ragnaros'
-          item.quality -= 1
-        end
-      end
+    next if item.name == 'Sulfuras, Hand of Ragnaros'
+    if item.quality < 50
+      multiple = quality_change_multiple(item)
+      update_item_quality(item, multiple)
+    end
+    update_item_sell_in(item)
+  end
+end
+
+def update_item_sell_in(item)
+  item.sell_in -= 1
+end
+
+def quality_change_multiple(item)
+  if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+    if item.sell_in <= 5
+      3
+    elsif item.sell_in <= 10
+      2
     else
-      if item.quality < 50
-        item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality += 1
-            end
-          end
-        end
-      end
+      1
     end
-    if item.name != 'Sulfuras, Hand of Ragnaros'
-      item.sell_in -= 1
+  elsif item.sell_in <= 0
+    2
+  else
+    1
+  end
+end
+
+def update_item_quality(item, multiple)
+  case item.name
+  when 'Aged Brie'
+    item.quality += [50 - item.quality, multiple].min
+  when 'Backstage passes to a TAFKAL80ETC concert'
+    if item.sell_in > 0
+      item.quality += [50 - item.quality, multiple].min
+    else
+      item.quality = 0
     end
-    if item.sell_in < 0
-      if item.name != "Aged Brie"
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            if item.name != 'Sulfuras, Hand of Ragnaros'
-              item.quality -= 1
-            end
-          end
-        else
-          item.quality = item.quality - item.quality
-        end
-      else
-        if item.quality < 50
-          item.quality += 1
-        end
-      end
-    end
+  else
+    item.quality -= [item.quality, multiple].min
   end
 end
 
 #----------------------------
 # DO NOT CHANGE THINGS BELOW
 #----------------------------
-
 Item = Struct.new(:name, :sell_in, :quality)
